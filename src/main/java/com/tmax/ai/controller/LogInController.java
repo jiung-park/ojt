28cdd43720cdb6eb1441f10c34e41decf4ca1bef
jiung-park <jiung_park@tmax.co.kr>
2022-07-18 16:15:40 +0900

    [feat] 회원 로그인 api 1차 구현

---
+++ src/main/java/com/tmax/ai/controller/LogInController.java
@@ -0,0 +1,25 @@
+package com.tmax.ai.controller;
+
+import com.tmax.ai.dto.LoginRequestDto;
+import com.tmax.ai.service.LogInService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/login")
+public class LogInController {
+
+    private final LogInService logInService;
+
+    @GetMapping
+    public ResponseEntity<Void> logIn(@RequestBody LoginRequestDto loginRequestDto) {
+        logInService.logIn(loginRequestDto);
+        return ResponseEntity.ok().build();
+    }
+
+}
